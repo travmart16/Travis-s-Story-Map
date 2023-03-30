5dfa577911fcbdd85627fa8ca19ba2941fe68243
travmart16 <128081185+travmart16@users.noreply.github.com>
2023-03-30 14:30:33 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qvIQjChH3Ebof_TOGry706LcdeatSENPHldgBKPvvcg/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qvIQjChH3Ebof_TOGry706LcdeatSENPHldgBKPvvcg/edit#gid=1033046795';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
